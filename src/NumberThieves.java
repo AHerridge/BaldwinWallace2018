88fc95ee2fd265e5d5339b4429dde1b5cd7f944e
Alex Herridge <alherridge@gmail.com>
2019-02-06 11:28:57 -0500

    Reformatted and finished 2

--- src/NumberThieves.java
+++ src/NumberThieves.java
@@ -1,36 +1,57 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Scanner;
 
-public class NumberThieves {
-    private static int getMin(Collection<Integer> nums) {
-        int min = 0;
+public class NumberThieves
+{
+	private static int getMin(Collection<Integer> nums)
+	{
+		int min = Integer.MAX_VALUE;
 
-        for (Integer num : nums)
-            if (num < min)
-                min = num;
+		for (Integer num : nums)
+			if (num < min)
+				min = num;
 
-        return min;
-    }
+		return min;
+	}
 
-    private static int getMax(Collection<Integer> nums) {
-        int max = 0;
+	private static int getMax(Collection<Integer> nums)
+	{
+		int max = 0;
 
-        for (Integer num : nums)
-            if (num > max)
-                max = num;
+		for (Integer num : nums)
+			if (num > max)
+				max = num;
 
-        return max;
-    }
+		return max;
+	}
 
-    private static int getMissing(Collection<Integer> nums) {
-        for (int i = getMin(nums); i < getMax(nums); i++)
-            if (!nums.contains(i))
-                return i;
+	private static int getMissing(Collection<Integer> nums)
+	{
+		for (int i = getMin(nums) + 1; i < getMax(nums); i++)
+			if (!nums.contains(i))
+				return i;
 
-        return getMax(nums) + 1;
-    }
+		return getMax(nums) + 1;
+	}
 
-    public static void main(String[] args) {
-        System.out.println(getMissing(Arrays.asList(1, 1, 2, 3, 5, 6)));
-    }
+	public static void main(String[] args)
+	{
+		Scanner console = new Scanner(System.in);
+		int numInputs = console.nextInt();
+
+		for (int i = 0; i < numInputs; i++)
+		{
+			ArrayList<Integer> nums = new ArrayList<>();
+			int length = console.nextInt();
+
+			for (int j = 0; j < length; j++)
+				nums.add(console.nextInt());
+
+			System.out.println("Case " + (i + 1) + ": " + getMissing(nums));
+		}
+
+		console.close();
+	}
 }
--- src/TwinBinaryEngine.java
+++ src/TwinBinaryEngine.java
@@ -1,33 +1,38 @@
 import java.util.Scanner;
 
-public class TwinBinaryEngine {
-    private static int log2(long value) {
-        return (int) (Math.log(value) / Math.log(2));
-    }
+public class TwinBinaryEngine
+{
+	private static int log2(long value)
+	{
+		return (int) (Math.log(value) / Math.log(2));
+	}
 
-    private static int numbersWithXDigits(int x) {
-        return x >= 3 ? (int) Math.pow(2, x - 1) : 2;
-    }
+	private static int numbersWithXDigits(int x)
+	{
+		return x >= 3 ? (int) Math.pow(2, x - 1) : 2;
+	}
 
-    private static long getAvgBinaryLength(int input) {
-        int log = log2(input);
-        long total = 0;
+	private static long getAvgBinaryLength(int input)
+	{
+		int log = log2(input);
+		long total = 0;
 
-        for (int i = 1; i <= log; i++)
-            total += numbersWithXDigits(i) * i;
+		for (int i = 1; i <= log; i++)
+			total += numbersWithXDigits(i) * i;
 
-        total += (input - Math.pow(2, log)) * (log + 1);
+		total += (input - Math.pow(2, log)) * (log + 1);
 
-        return total / input;
-    }
+		return total / input;
+	}
 
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        int numInputs = console.nextInt();
+	public static void main(String[] args)
+	{
+		Scanner console = new Scanner(System.in);
+		int numInputs = console.nextInt();
 
-        for (int i = 0; i < numInputs; i++)
-            System.out.println("Case " + i + ": " + getAvgBinaryLength(console.nextInt()));
+		for (int i = 0; i < numInputs; i++)
+			System.out.println("Case " + i + ": " + getAvgBinaryLength(console.nextInt()));
 
-        console.close();
-    }
+		console.close();
+	}
 }
